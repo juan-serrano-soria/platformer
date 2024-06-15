998b8af7b273064d7b358a6d8a0909ddb1b689a8
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2024-06-16 03:50:32 +0900

    Add jump and movement prototype

--- main.lua
+++ main.lua
@@ -4,10 +4,18 @@ function love.load()
     world = wf.newWorld(0, 500)
 
     player = world:newRectangleCollider(350, 100, 80, 80)
-    
+    ground = world:newRectangleCollider(100, 400, 600, 100)
+    ground:setType("static")
 end
 
 function love.update(dt)
+    -- Player movement constrained by a max velocity
+    local px, py = player:getLinearVelocity()
+    if love.keyboard.isDown('left') and px > -300 then
+        player:applyForce(-5000, 0)
+    elseif love.keyboard.isDown('right') and px < 300 then
+        player:applyForce(5000, 0)
+    end
     -- Update the world physics
     world:update(dt)
 end
@@ -15,4 +23,11 @@ end
 function love.draw()
     -- Draw the colliders
     world:draw()
+end
+
+function love.keypressed(key)
+    -- Player jump
+    if key == "up" then
+        player:applyLinearImpulse(0, -5000)
+    end
 end
