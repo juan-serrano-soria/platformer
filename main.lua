6380c15aa46456e6b879313146d3f492d625682d
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2024-06-15 23:16:32 +0900

    Add basic world with gravity

--- main.lua
+++ main.lua
@@ -1,11 +1,18 @@
 function love.load()
+    wf = require "libs/windfield"
 
+    world = wf.newWorld(0, 500)
+
+    player = world:newRectangleCollider(350, 100, 80, 80)
+    
 end
 
 function love.update(dt)
-
+    -- Update the world physics
+    world:update(dt)
 end
 
 function love.draw()
-
+    -- Draw the colliders
+    world:draw()
 end
