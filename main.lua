ee980e63a74925dc4bb960e85b1f1453728bafa3
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2024-06-16 13:01:38 +0900

    Added jump restriction

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ function love.load()
     player = world:newRectangleCollider(350, 100, 80, 80)
     ground = world:newRectangleCollider(100, 400, 600, 100)
     ground:setType("static")
+
+    canJump = true
 end
 
 function love.update(dt)
@@ -15,6 +17,11 @@ function love.update(dt)
         player:applyForce(-5000, 0)
     elseif love.keyboard.isDown('right') and px < 300 then
         player:applyForce(5000, 0)
+    end
+    -- Check if player is on the ground
+    canJump = false
+    if player:isTouching(ground.body) then
+        canJump = true
     end
     -- Update the world physics
     world:update(dt)
@@ -27,7 +34,8 @@ end
 
 function love.keypressed(key)
     -- Player jump
-    if key == "up" then
+    if key == "up" and canJump then
         player:applyLinearImpulse(0, -5000)
+        canJump = false
     end
 end
