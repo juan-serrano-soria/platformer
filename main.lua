ae3bdb136dc35a138841a9bca3ce6d045aa5f850
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2024-06-18 22:00:20 +0900

    Add jump-through grounds

--- main.lua
+++ main.lua
@@ -13,25 +13,57 @@ function love.load()
         world:newRectangleCollider(335, 425, 20, 10),
         world:newRectangleCollider(375, 415, 20, 10),
         world:newRectangleCollider(350, 355, 30, 10),
-        world:newRectangleCollider(435, 245, 30, 10),
         world:newRectangleCollider(550, 430, 30, 10),
+        world:newRectangleCollider(435, 245, 30, 10),
+        world:newRectangleCollider(285, 215, 120, 10),
     }
 
     bouncyGrounds = {
         world:newRectangleCollider(650, 430, 30, 10),
         world:newRectangleCollider(700, 280, 30, 10),
+        world:newRectangleCollider(100, 210, 10, 10),
     }
 
-    for _, groundTable in ipairs({grounds, bouncyGrounds}) do
+    jumpThroughGrounds = {
+        world:newRectangleCollider(285, 165, 50, 10),
+        world:newRectangleCollider(285, 115, 50, 10),
+        world:newRectangleCollider(10, 65, 100, 10),
+    }
+
+    -- Set collision classes
+    world:addCollisionClass('Player')
+    world:addCollisionClass('JumpThrough')
+
+    for _, groundTable in ipairs({grounds, bouncyGrounds, jumpThroughGrounds}) do
         for _, ground in ipairs(groundTable) do
             ground:setType("static")
         end
     end
 
+    -- Set collision callbacks
+    player:setCollisionClass('Player')
+    for _, ground in ipairs(jumpThroughGrounds) do
+        ground:setCollisionClass('JumpThrough')
+    end
+
+    -- Set pre-solve callback for jump-through platforms
+    player:setPreSolve(function(collider_1, collider_2, contact)
+        if collider_1.collision_class == 'Player' and collider_2.collision_class == 'JumpThrough' then
+            local _, vy = collider_1:getLinearVelocity()
+            if vy < 0 then
+                contact:setEnabled(false) -- If player is moving down, disable the collision
+            else
+                contact:setEnabled(true) -- If player is moving up, enable the collision
+            end
+        end
+    end)
+
     rightWall = world:newRectangleCollider(790, 0, 10, 600)
     rightWall:setType("static")
     leftWall = world:newRectangleCollider(0, 0, 10, 600)
     leftWall:setType("static")
+    topWall = world:newRectangleCollider(0, 0, 800, 10)
+    topWall:setType("static")
 
     canJump = true
 end
@@ -58,6 +90,14 @@ function love.update(dt)
     for i, ground in ipairs(bouncyGrounds) do
         if player:isTouching(ground.body) then
             player:applyLinearImpulse(0, -50)
+            break
+        end
+    end
+
+    -- Check if player is on the jump-through ground
+    for _, ground in ipairs(jumpThroughGrounds) do
+        if player:isTouching(ground.body) then
+            canJump = true
             break
         end
     end
